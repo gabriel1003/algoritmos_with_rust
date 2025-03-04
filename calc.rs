36900e9e4d535c9d5438c6f686275e2c61e74f4a
gabriel1003 <gabriel.samersla97@gmail.com>
2025-03-04 11:32:24 -0300

    fiat: adiciona um arquivo que contem as operacoes de soma, subtracao e divisao usando numeros inteiros e flutuantes

---
+++ calc.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+// adição
+    println!("1 + 2 = {} e 8 - 5 = {} e 15 * 3 = {}", 1u32 + 2, 8i32 - 5, 15 * 3);
+
+// inteiros e flutuantes
+    println!("9 / 2 = {} e 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
+}
+
