070e2a6b39bf624860fc56d20a5606a2e86f4267
gabriel1003 <gabriel.samersla97@gmail.com>
2025-03-04 18:17:02 -0300

    feat: adiciona o arquivo tupla.rs
    este arquivo contem a declaracao de uma tupla com tres elementos.

---
+++ tupla.rs
@@ -0,0 +1,8 @@
+
+
+fn main() {
+// uma tupla é um valor imutável definido pelo seu tamanho.
+let tupla = ('E', 5i32, true);
+println!("Aqui temos uma tupla com três elementos. primeiro elemento: '{}', segundo elemento: {}, e terceiro elemento: {}", tupla.0, tupla.1, tupla.2);
+}
+
